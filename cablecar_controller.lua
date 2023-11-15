78aefb894e72bb2ba727511587d3375af8ddaac7
Jim Shield <gothicjim@gmail.com>
2023-11-15 15:46:40 +0000

    Rewritten, Optimized, use Polyzones

--- cablecar_controller.lua
+++
@@ -1,6 +0,0 @@
-RegisterServerEvent("omni:cablecar:host:sync")
-AddEventHandler("omni:cablecar:host:sync", function(index, state)
-    if source == tonumber(GetPlayers()[1]) then
-        TriggerClientEvent("omni:cablecar:forceState", -1, index, state)
-    end
-end)
